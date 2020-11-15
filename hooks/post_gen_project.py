f465c67b486ebc740d5a82cbf61dee0f18c349ac
HarryMWinters <harrymcwinters@gmail.com>
2020-11-14 23:04:14 -0800

    Fix formatting for isort and black

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,7 +8,19 @@ def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    if 'Not open source' == '{{ cookiecutter.open_source_license }}':
-        remove_file('LICENSE')
+    if "Not open source" == "{{ cookiecutter.open_source_license }}":
+        remove_file("LICENSE")
+
+    os.system("git init")
+    os.system("git add *")
+    os.system(
+        "git add "
+        f"{PROJECT_DIRECTORY}/.flake8 "
+        f"{PROJECT_DIRECTORY}/.gitignore "
+        f"{PROJECT_DIRECTORY}/.isort.cfg "
+        f"{PROJECT_DIRECTORY}/.pre-commit-config.yaml "
+    )
+    os.system("pre-commit install")
+
